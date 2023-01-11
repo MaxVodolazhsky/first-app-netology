6db0786cc2c8746cf68e4b2597d846500590470b
mrx <mrx@MacBook-Pro-mrx.local>
2023-01-11 20:48:16 +0300

    adding min function and average function

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[] {200, 2, 4, 20});
+        SalesManager salesManager = new SalesManager(new int[] {10, 1, 2});
 
         int max = salesManager.max();
         System.out.println(max);
+
+        int min = salesManager.min();
+        System.out.println(min);
+
+        int average = salesManager.average();
+        System.out.println(average);
 
     }
 }
