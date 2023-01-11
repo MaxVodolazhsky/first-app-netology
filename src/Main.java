11f44ec0773eb3cffbda3924b002b53df5a984b4
mrx <mrx@MacBook-Pro-mrx.local>
2023-01-11 20:37:10 +0300

    resolve conflict

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+
         SalesManager salesManager = new SalesManager(new int[] {10, 1, 2});
+
 
         int max = salesManager.max();
         System.out.println(max);
